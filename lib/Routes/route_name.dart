4593b974673efce4f688ab901c7329a59e783c0f
SaadAhmed <99617062+SaadAhmed012@users.noreply.github.com>
2024-05-16 02:47:24 +0500

    Added RouteNames

---
+++ lib/Routes/route_name.dart
@@ -0,0 +1,6 @@
+class RouteNames {
+  static const String splashScreen = 'splash_screen';
+  static const String loginScreen = 'login_screen';
+  static const String signUpScreen = 'signUp_screen';
+  static const String postScreen = 'post_screen';
+}
