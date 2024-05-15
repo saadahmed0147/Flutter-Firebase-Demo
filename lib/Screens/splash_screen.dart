a3163f166d98a7d24f97068bcd62dc147674734c
SaadAhmed <99617062+SaadAhmed012@users.noreply.github.com>
2024-05-16 02:49:04 +0500

    Added SplashScreen

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_1/Firebase_services/splash_services.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  SplashServices splashServices = SplashServices();
+  @override
+  void initState() {
+    splashServices.isLogin(context);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Icon(Icons.timer),
+      ),
+    );
+  }
+}
