2159ff09fc87175926effbcad3ebdf24a6281e88
SaadAhmed <99617062+SaadAhmed012@users.noreply.github.com>
2024-05-16 02:46:31 +0500

    Added SplashServices

---
+++ lib/Firebase_services/splash_services.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:firebase_1/Routes/route_name.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SplashServices {
+  void isLogin(BuildContext context) {
+    final firebaseAuth = FirebaseAuth.instance;
+    final user = firebaseAuth.currentUser;
+
+    if (user != null) {
+      debugPrint(user.toString());
+      Timer(const Duration(seconds: 3), () {
+        Navigator.pushNamed(context, RouteNames.postScreen);
+      });
+    } else {
+      Timer(const Duration(seconds: 3), () {
+        Navigator.pushNamed(context, RouteNames.loginScreen);
+      });
+    }
+  }
+}
