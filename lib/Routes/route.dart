c603a33e0333dee91f355166f0b2f5227ef88747
SaadAhmed <99617062+SaadAhmed012@users.noreply.github.com>
2024-05-23 02:47:20 +0500

    Added otpVerificationScreen

--- lib/Routes/route.dart
+++ lib/Routes/route.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_1/Posts/post_screen.dart';
 import 'package:firebase_1/Routes/route_name.dart';
 import 'package:firebase_1/Screens/login_screen.dart';
+import 'package:firebase_1/Screens/otp_verification_screen.dart';
+import 'package:firebase_1/Screens/phone_number_input_screen.dart';
 import 'package:firebase_1/Screens/signup_screen.dart';
 import 'package:firebase_1/Screens/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -23,6 +25,17 @@ class Routes {
       case RouteNames.postScreen:
         return MaterialPageRoute(
           builder: (context) => const PostScreen(),
+        );
+      case RouteNames.phoneNumberInputScreen:
+        return MaterialPageRoute(
+          builder: (context) => const PhoneNumberInputScreen(),
+        );
+      case RouteNames.otpVerificationScreen:
+        final arg = settings.arguments as Map<String, String>;
+        return MaterialPageRoute(
+          builder: (context) => OTPVerificationScreen(
+            verificationId: arg['verificationId']!,
+          ),
         );
 
       default:
