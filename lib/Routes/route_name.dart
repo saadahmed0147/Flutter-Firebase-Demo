b4937a9d7a9dce374209b7b3bfcb3a41a694f83e
SaadAhmed <99617062+SaadAhmed012@users.noreply.github.com>
2024-05-23 02:47:02 +0500

    Added otpVerificationScreen

--- lib/Routes/route_name.dart
+++ lib/Routes/route_name.dart
@@ -3,4 +3,6 @@ class RouteNames {
   static const String loginScreen = 'login_screen';
   static const String signUpScreen = 'signUp_screen';
   static const String postScreen = 'post_screen';
+  static const String phoneNumberInputScreen = 'phone_number_input_screen';
+  static const String otpVerificationScreen = 'otp_verification_screen';
 }
