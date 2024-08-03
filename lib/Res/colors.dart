cb9901e7e9b3044dff40c7c148a3c006a617d4fd
saadahmed0147 <saadahmed0147@gmail.com>
2024-08-03 02:57:10 -0700

    Added Colors

--- lib/Res/colors.dart
+++ lib/Res/colors.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const whiteColor = Colors.white;
   static const blackColor = Colors.black;
+  static const lightGreenColor = Color.fromRGBO(180, 214, 180, 1);
+  static const greenColor = Color(0xff457846);
+  static const btnColor = Color(0xff59ca7f);
+
 }
