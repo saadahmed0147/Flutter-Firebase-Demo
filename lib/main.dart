4526b7f94a41c6c54fad693d4634e8c27ddc8d88
saadahmed0147 <saadahmed0147@gmail.com>
2024-08-03 02:55:01 -0700

    Added size

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+
+late Size mq;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
